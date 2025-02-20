3d6ffb1148f29f764506c896951450bd5d083b7f
Ruhaan18 <raiyandalal8@gmail.com>
2025-02-20 14:23:40 +0530

    lol i am noob

--- inference.py
+++ inference.py
@@ -1,7 +1,10 @@
 from main import build_model_main
 from util.slconfig import SLConfig
 
+import torch
+
 args = SLConfig.fromfile('config/DINO/DINO_4scale.py')
+args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = build_model_main(args)
 
 for name, param in model.named_parameters():
